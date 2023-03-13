cbe189c13c9885d3c3f154779edb912c525343cb
Fahad Alothman <dev.fahad.alothman@gmail.com>
2023-03-13 03:11:02 +0300

    complete Function Overrides

--- src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
+++ src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
@@ -1,12 +1,25 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function runGenerator(generator: unknown) {
+function runGenerator(generator: () => string): string;
+function runGenerator(generator: { run: () => string }): string;
+function runGenerator(
+  generator: (() => string) | { run: () => string }
+): string {
   if (typeof generator === "function") {
     return generator();
   }
   return generator.run();
 }
+
+// or
+
+const runGenerator2 = (generator: (() => string) | { run: () => string }) => {
+  if (typeof generator === "function") {
+    return generator();
+  }
+  return generator.run();
+};
 
 it("Should accept an object where the generator is a function", () => {
   const result = runGenerator({
@@ -14,8 +27,14 @@ it("Should accept an object where the generator is a function", () => {
   });
 
   expect(result).toBe("hello");
-
   type test1 = Expect<Equal<typeof result, string>>;
+
+  const result2 = runGenerator2({
+    run: () => "hello",
+  });
+
+  expect(result2).toBe("hello");
+  type test2 = Expect<Equal<typeof result2, string>>;
 });
 
 it("Should accept an object where the generator is a function", () => {
@@ -24,4 +43,10 @@ it("Should accept an object where the generator is a function", () => {
   expect(result).toBe("hello");
 
   type test1 = Expect<Equal<typeof result, string>>;
+
+  const result2 = runGenerator2(() => "hello");
+
+  expect(result2).toBe("hello");
+
+  type test2 = Expect<Equal<typeof result2, string>>;
 });
